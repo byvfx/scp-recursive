72f95d42e071213d3d50008d3552146c1a9113df
Brandon Young <67305844+byvfx@users.noreply.github.com>
2025-09-16 22:35:52 -0700

    added keyboard functionality to able to grab focus, its currently a workaround since mouse input doesnt work

--- scenes/ui/desktop/desktop.gd
+++ scenes/ui/desktop/desktop.gd
@@ -1,5 +1,13 @@
 extends Control
 
-
+@onready var start = $Panel/Start
+func _ready() -> void:
+	if start:
+		start.grab_focus()
+		
 func _on_start_pressed() -> void:
 	print("START")
+
+
+func _on_file_explorer_pressed() -> void:
+	print("FILE EXPLORER")# Replace with function body.
